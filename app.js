32223820ab3270973a34177939af2e961d94c705
jay-web <jay.developer12@gmail.com>
2021-02-20 18:00:57 +0530

    Added jwt token and cookie options, with logout controller

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const userRoutes = require("./routes/userRoutes");
 const app = express();
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 
 app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 
 app.get("/api/v1/chats", (req, res, next) => {
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,73 +1,109 @@
 const USER = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
+//  CREATE THE TOKEN TO SEND FOR AUTHENTICATION
+const createToken = (userId) => {
+  return jwt.sign({ id: userId, name: "humming" }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+// SEND THE TOKEN WITH COOKIES
+
+const sendToken = async (user, req, res, statusCode) => {
+  const token = createToken(user.id);
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT__COOKIES_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    secure: req.secure || req.headers["x-forwarded-proto"] === "https",
+  };
+
+  if (process.env.NODE_ENV === "development") {
+    cookieOptions.secure = false;
+  }
+
+  res.cookie("jwt", token, cookieOptions);
+
+    return  res.status(statusCode).json({
+      status: "success",
+      token
+
+  })
+};
 
 // Middleware For signup
 exports.createUser = async (req, res, next) => {
-   try{
-    const {name, username, email, password, passwordConfirm } = req.body;
+  try {
+    const { name, username, email, password, passwordConfirm } = req.body;
     const userInfo = {
-        name,
-        username,
-        email,
-        password,
-        passwordConfirm
-    }
+      name,
+      username,
+      email,
+      password,
+      passwordConfirm,
+    };
 
     const newUser = await USER.create(userInfo);
-    
-    res.status(200).json({
-        status: "success",
-        body:{
-            user: newUser
-        }
-    })
-   }catch(error){
-        res.status(500).json({
-            status: "failed",
-            message: error.message
-        })
-   }
-    
-}
 
+    sendToken(newUser, req, res, 201)
+  } catch (error) {
+    res.status(500).json({
+      status: "failed",
+      message: error.message,
+    });
+  }
+};
 
 // Middleware for login
 exports.login = async (req, res, next) => {
-    try{
-        const {username, password} = req.body;
-        console.log(username, password);
+  try {
+    const { username, password } = req.body;
+    console.log(username, password);
 
-        // Check username and password both are provided by user
-        if(!username || !password){
-            return res.status(400).json({
-                status: "failed",
-                message: "Please provide username and password"
-            })
-        }
-
-        // Check provided details are valid or not
-        const user = await USER.findOne({ username: username}).select("+password");
-        
-        const isPasswordCorrect = await user.correctPassword(password, user.password);
-
-        if(!user || !isPasswordCorrect){
-            return res.status(401).json({
-                status: "failed",
-                message: "Incorrect Username or password"
-            })
-        }
-
-      
-        res.status(200).json({
-            status: "success",
-            body:{
-                user: "user"
-            }
-        })
-    }catch(error){
-        res.status(500).json({
-            status: "failed",
-            message: error.message
-        })
+    // Check username and password both are provided by user
+    if (!username || !password) {
+      return res.status(400).json({
+        status: "failed",
+        message: "Please provide username and password",
+      });
     }
+
+    // Check provided details are valid or not
+    const user = await USER.findOne({ username: username }).select("+password");
+
+    const isPasswordCorrect = await user.correctPassword(
+      password,
+      user.password
+    );
+
+    if (!user || !isPasswordCorrect) {
+      return res.status(401).json({
+        status: "failed",
+        message: "Incorrect Username or password",
+      });
+    }
+
+    sendToken(user, req, res, 201)
+  } catch (error) {
+    res.status(500).json({
+      status: "failed",
+      message: error.message,
+    });
+  }
+};
+
+// Middleware for log out
+exports.logout = async (req, res, next) => {
+    res.cookie("jwt", "logout", {
+        expiresIn: new Date(Date.now() + 10 * 1000),
+        httpOnly: true
+    });
+
+    res.status(200).json({
+        status: "success",
+
+    })
 }
